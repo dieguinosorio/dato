47365b0c594957e67ab3215d387e066c9e0d6f8c
dlaravel <dlaravel@dlaravel-VirtualBox>
2019-10-30 02:02:32 -0500

    comit: ajustes iniciales

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -90,65 +90,165 @@ class HomeController extends Controller {
 //            $Mensaje = "El campo social no se diligencio correctamente";
 //            $Status = "400";
 //        }
-                $OpCity = $request->input('OpCity');
-                $Firtsname= $request->input('Firtsname');
-                $midname= $request->input('midname');
-                $lastname= $request->input('lastname');
-                $tel = $request->input('tel');
-                $gender = $request->input('gender');
-                $fhbirth = $request->input('fhbirth');
-                $dir = $request->input('dir');
-                $dir2 = $request->input('dir2');
-                $city = $request->input('city');
-                $state = $request->input('state');
-                $zipcode = $request->input('zipcode');
-                $marital = $request->input('marital');
-                $email = $request->input('email');
-                $tpdoc= $request->input('tpdoc');
-                $socialnum = $request->input('socialnum');
-                $numdep = $request->input('numdep');
-                $contactemer = $request->input('contactemer');
-                $surnamecon = $request->input('surnamecon');
-                $telcon = $request->input('telcon');
-                $area = $request->input('area');
-                $namesur= $request->input('namesur');
-                $job1 = $request->input('job1');
-                $job2 = $request->input('job2');
-                $job3 = $request->input('job3');
-                $job4 = $request->input('job4');
-                $job5 = $request->input('job5');
-                $job6 = $request->input('job6');
-                $job7 = $request->input('job7');
-                $job8 = $request->input('job8');
-                $job9 = $request->input('job9');
-                $job10 = $request->input('job10');
-                
-                $mil = $request->input('mil');
-                $dem = $request->input('dem');
-                $demc = $request->input('demc');
+        $OpCity = $request->input('OpCity');
+        $Firtsname = $request->input('Firtsname');
+        $midname = $request->input('midname');
+        $lastname = $request->input('lastname');
+        $tel = $request->input('tel');
+        $gender = $request->input('gender');
+        $fhbirth = $request->input('fhbirth');
+        $dir = $request->input('dir');
+        $dir2 = $request->input('dir2');
+        $city = $request->input('city');
+        $state = $request->input('state');
+        $zipcode = $request->input('zipcode');
+        $marital = $request->input('marital');
+        $email = $request->input('email');
+        $tpdoc = $request->input('tpdoc');
+        $socialnum = $request->input('socialnum');
+        $numdep = $request->input('numdep');
+        $contactemer = $request->input('contactemer');
+        $surnamecon = $request->input('surnamecon');
+        $telcon = $request->input('telcon');
+        $area = $request->input('area');
+        $namesur = $request->input('namesur');
+        $job1 = $request->input('job1');
+        $job2 = $request->input('job2');
+        $job3 = $request->input('job3');
+        $job4 = $request->input('job4');
+        $job5 = $request->input('job5');
+        $job6 = $request->input('job6');
+        $job7 = $request->input('job7');
+        $job8 = $request->input('job8');
+        $job9 = $request->input('job9');
+        $job10 = $request->input('job10');
+
+        $mil = $request->input('mil');
+        $dem = $request->input('dem');
+        $demc = $request->input('demc');
 //                clause:clause,
 //                term:term,
 //                image_path :image_path,
 //                image_path2 :image_path2,
-                $acept = $request->input('acept');
+        $acept = $request->input('acept');
+        $StrMensaje ='';
+        if($OpCity=='(sel)'){
+            $StrMensaje.="<li>Selected your option citizen<li/>";
+        }
+        if($Firtsname =='') {
+            $StrMensaje.="<li>Insert your firstname<li/>";
+        }
+        if($midname =='') {
+            $StrMensaje.="<li>Insert your midname<li/>";
+        }
+        if($lastname =='') {
+            $StrMensaje.="<li>Insert your lastname<li/>";
+        }
+        if($tel =='') {
+            $StrMensaje.="<li>Insert your telephone<li/>";
+        }
+        if($gender =='(sel)') {
+            $StrMensaje.="<li>Insert your gender <li/>";
+        }
+        if($fhbirth =='') {
+            $StrMensaje.="<li>Insert your Date birth<li/>";
+        }
+        if($dir=='') {
+            $StrMensaje.="<li>Selected your direction<li/>";
+        }
+        if($dir2=='') {
+            $StrMensaje.="<li>Insert your direction 2<li/>";
+        }
+        if($city=='') {
+           $StrMensaje.="<li>Insert your city<li/>"; 
+        }
+        if($state=='(sel)') {
+            $StrMensaje.="<li>Insert your state<li/>";
+        }
+        if($zipcode=='') {
+            $StrMensaje.="<li>Insert your zipcode<li/>";
+        }
+        if($marital=='(sel)') {
+            $StrMensaje.="<li>Selected your marital status<li/>";
+        }
+        if($email=='') {
+            $StrMensaje.="<li>Insert your email<li/>";
+        }
+        if($tpdoc=='(sel)') {
+            $StrMensaje.="<li>select your option document identity<li/>";
+        }
+        if($socialnum=='') {
+            $StrMensaje.="<li>Insert your social id<li/>";
+        }
+        if($numdep=='') {
+            $StrMensaje.="<li>Insert your number dependients<li/>";
+        }
+        if($contactemer=='') {
+            $StrMensaje.="<li>Insert name contact emergency<li/>";
+        }
+        if($surnamecon =='') {
+            $StrMensaje.="<li>Insert surname contact emergency<li/>";
+        }
+        if($telcon=='') {
+            $StrMensaje.="<li>Insert telephone contact emergency<li/>";
+        }
+        if($area=='') {
+            $StrMensaje.="<li>Insert your area<li/>";
+        }
+        if($namesur=='') {
+            $StrMensaje.="<li>Insert your name supervisor<li/>";
+        }
+//        if($job1) {
+//            $StrMensaje.="<li>Insert your option citizen<li/>";
+//        }
+//        if($job2) {
+//            $StrMensaje.="<li>Selected your option citizen<li/>";
+//        }
+//        if($job3) {
+//            $StrMensaje.="<li>Selected your option citizen<li/>";
+//        }
+//        if($job4) {
+//            $StrMensaje.="<li>Selected your option citizen<li/>";
+//        }
+//        if($job5) {
+//            $StrMensaje.="<li>Selected your option citizen<li/>";
+//        }
+//        if($job6 ) {
+//            $StrMensaje.="<li>Selected your option citizen<li/>";
+//        }
+//        if($job7 ) {
+//            $StrMensaje.="<li>Selected your option citizen<li/>";
+//        }
+//        if($job8) {
+//            $StrMensaje.="<li>Selected your option citizen<li/>";
+//        }
+//        if($job9) {
+//            $StrMensaje.="<li>Selected your option citizen<li/>";
+//        }
+//        if($job10) {
+//            $StrMensaje.="<li>Selected your option citizen<li/>";
+//        }
+
+        if($mil) {
+            $StrMensaje.="<li>Selected your option (Are you eligible to work on military bases in USA)<li/>";
+        }
+        if($dem) {
+            $StrMensaje.="<li>Selected your option (Have you ever been involved in a lawsuit with any previous employer)<li/>";
+        }
+        if($demc) {
+            $StrMensaje.="<li>Selected your option (Have you ever been involved in a Workers Compensation Claim?)<li/>";
+        }
+        if($acept) {
+            $StrMensaje.="<li>Selected your option (Are you willing to travel if there are qualified work positions in another city or state?)<li/>";
+        }
+        
+        
 
         return response()->json([
                     'Campo' => '',
                     'status' => 200,
-                    'message' => $OpCity."-".$Firtsname."-".$midname."-".$lastname."-".$tel."-".$gender."-".$fhbirth."-".$dir."-".$dir2.
-                     $city ."-".
-                    $state ."-".
-                    $zipcode ."-".
-                    $marital ."-".
-                    $email  ."-".
-                    $tpdoc."-".
-                    $socialnum ."-".
-                    $numdep ."-".
-                    $contactemer ."-".
-                    $surnamecon ."-".
-                    $telcon ."-".
-                    $area ."-".
-                    $namesur."-<li>".$job1."</li>-".$job2."-".$job3."-".$job4."-".$job5."-".$job6."-".$job7."-".$job8."-".$job9."-".$job10
-            ]);
+                    'message' => $StrMensaje
+        ]);
     }
+
 }
