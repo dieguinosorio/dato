a480017a8eab6eda67bc81cd56a5ee848527f872
dlaravel <dlaravel@dlaravel-VirtualBox>
2019-12-07 07:18:53 -0500

    comit: Nuevos ajustes

--- resources/views/empresa/configuration.blade.php
+++ resources/views/empresa/configuration.blade.php
@@ -76,7 +76,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Actualizar Datos
+                                    Update
                                 </button>
                             </div>
                         </div>
