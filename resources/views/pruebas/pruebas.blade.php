25223621a98164a9b5bf0594ee77cf58e391b0db
dlaravel <dlaravel@dlaravel-VirtualBox>
2019-12-12 06:49:53 -0500

    comit: Se crea email de registro

--- resources/views/pruebas/pruebas.blade.php
+++ resources/views/pruebas/pruebas.blade.php
@@ -7,9 +7,24 @@
         <div class="card-header">Enviar Mail</div>
         <div class="card-body">
             <form method="GET" action="{{action('HomeController@PruebaMail')}}">
-                <input class="form-control" type="text" name="titulo" /><br>
-                <input class="form-control" type="text" name="mensaje" />
-                <input type="submit" class="btn btn-success" value='Enviar Mail'/>
+                <div class="form-group row">
+                    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Title') }}</label>
+                    <div class="col-md-6">
+                        <input class="form-control" type="text" name="titulo" /><br>
+                    </div>
+                    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Menssage') }}</label>
+                    <div class="col-md-6">
+                        <textarea class="form-control"  name="mensaje" ></textarea><br>
+                    </div>
+                    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
+                    <div class="col-md-6">
+                        <input class="form-control" type="email" name="email" /><br>
+                    </div>
+                    <div class="col-md-6">
+                        <input type="submit" class="btn btn-success" value='Enviar Mail'/>
+                    </div>
+                </div>
+
             </form>
         </div>
     </div>
