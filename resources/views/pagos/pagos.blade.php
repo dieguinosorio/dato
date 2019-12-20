31531278bdd75336e0432b4600b02d8ff5834dcc
dlaravel <dlaravel@dlaravel-VirtualBox>
2019-12-20 01:31:41 -0500

    comit: Se crean nuevos cambios para formas de pago

--- resources/views/pagos/pagos.blade.php
+++ resources/views/pagos/pagos.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-4">
             <div class="alert-primary card-header"><strong>Payments</strong>
             </div>
             <div class="card-body">
@@ -14,15 +14,15 @@
                     @csrf
                     <div class="row">
                         <div class="col-auto">
-                            <label>How much want to pay ?</label>
-                            <input type="number" min="5"  step="0.01" class="form-control" name="value" value="{{mt_rand(500,100000)/100}}" required>
+                            <label> Total to pay </label>
+                            <h1><label class="alert-danger">$ {{$plan->Price}} USD</label></h1>
+                            <input type="hidden" min="5"  step="0.01" class="form-control" name="value" value="{{$plan->Price}}">
                             <small class="form-text text-muted">
                                 Use values with up to two decimal positions.
                             </small>
                         </div>
                         <div class="col-auto">
-                            <label>Currency</label>
-                            <select class="custom-select" id="TpMoneda" name="moneda">
+                            <select class="custom-select" id="TpMoneda" name="moneda" style="display: none;">
                                 @if(isset($monedas))
                                     @foreach($monedas as $tipo)
                                     <option value="{{$tipo->iso}}" 
@@ -38,6 +38,8 @@
                         
                     </div>
                     <div class="row mt-3">
+                        
+                        
                         <div class="col-md">
                             <label>Selected the desired payment platform:</label>
                             <div class="form-group" id='toggler'>
@@ -48,7 +50,7 @@
                                        data-toggle="collapse"    
                                     >
                                         <input type="radio" name="payment_platform" value="{{$PayForm->id}}" required>
-                                        <img class="img-thumbnail" src="{{$PayForm->image}}">
+                                        <img class="img-thumbnail" src="{{action("HomeController@getImagePublic",['filename'=>strtolower($PayForm->name).".jpg"])}}">
                                     </label>
                                     @endforeach
                                 </div>
